e74bf5177f4944638c25501faa117b98f58a8ef1
Jezreel Pangasian <20240030824@my.xu.edu.ph>
2026-04-24 18:46:14 +0800

    add navigation logic

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("App started");
+let currentPage = "intro";
+
+function goPage(target) {
+    document.getElementById(currentPage).classList.remove("active");
+    document.getElementById(target).classList.add("active");
+    currentPage = target;
+}
